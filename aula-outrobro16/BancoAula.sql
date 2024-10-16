9985642b959d38d33a4a196013519233b5cd265d
KauePiotto <kauepiotto@gmail.com>
2024-10-16 09:24:35 -0300

    Aula-bd

--- aula-outrobro16/BancoAula.sql
+++ aula-outrobro16/BancoAula.sql
@@ -1,1 +1,15 @@
-CREATE SCHEMA `senac`;
+INSERT iNTO alunos (idalunos,alunosNome, alunosTelefone,alunosNota )
+VALUES ('','João Silva','(13) 99795-6300', '7,5');
+
+INSERT iNTO alunos (idalunos,alunosNome, alunosTelefone,alunosNota )
+VALUES ('','Kaue Piotto','(13) 12356-0000 ', '10,0');
+
+INSERT iNTO alunos (idalunos,alunosNome, alunosTelefone,alunosNota )
+VALUES ('','Fernando Tavares','(13) 55096-0000 ', '9,5');
+
+select * from alunos;
+
+INSERT iNTO funcionarios (idFuncionaros,nomeFuncionarios,idadeFunarios,setorFuncionarios)
+VALUES ('','Fernando Tavares','20', 'Desevolvedor Junior');
+
+select * from funcionarios;
